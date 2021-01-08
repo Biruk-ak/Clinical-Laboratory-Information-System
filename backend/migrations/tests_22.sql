5ac078ead14ebb9566391d5ee9f9796e2a4b8a71
Biruk-ak <birukaklilu0110@gmail.com>
2021-01-08 10:20:00 +0300

    Add Lab Tests catalog domain models, migrations, and docs

---
+++ backend/migrations/tests_22.sql
@@ -0,0 +1,24 @@
+-- Migration for tests slice 22
+CREATE TABLE IF NOT EXISTS tests_22_records (
+  id            TEXT PRIMARY KEY,
+  external_code TEXT NOT NULL,
+  display_name  TEXT NOT NULL,
+  status        TEXT NOT NULL,
+  priority      INTEGER NOT NULL DEFAULT 0,
+  facility_id   TEXT NOT NULL,
+  created_by    TEXT NOT NULL,
+  updated_by    TEXT NOT NULL,
+  notes         TEXT NOT NULL DEFAULT '',
+  metadata_json TEXT NOT NULL DEFAULT '{}',
+  version       BIGINT NOT NULL DEFAULT 1,
+  is_active     BOOLEAN NOT NULL DEFAULT TRUE,
+  created_at    TIMESTAMPTZ NOT NULL,
+  updated_at    TIMESTAMPTZ NOT NULL,
+  archived_at   TIMESTAMPTZ NULL
+);
+
+CREATE INDEX IF NOT EXISTS idx_tests_22_facility_active
+  ON tests_22_records (facility_id, is_active, updated_at DESC);
+
+CREATE INDEX IF NOT EXISTS idx_tests_22_external_code
+  ON tests_22_records (facility_id, external_code);
---
+++ docs/modules/tests.md
@@ -0,0 +1,5 @@
+# LabTest module
+
+Test catalog, panels, methodologies, and turnaround targets
+
+Generated domain slices: 28
