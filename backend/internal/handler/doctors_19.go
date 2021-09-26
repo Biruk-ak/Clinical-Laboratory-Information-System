7cc00e8ccdb80086c469d26a7372f978bcf140ce
Biruk-ak <birukaklilu0110@gmail.com>
2021-09-26 12:20:00 +0300

    Expose Doctors directory REST API handlers

---
+++ backend/internal/handler/doctors_19.go
@@ -0,0 +1,153 @@
+package handler
+
+
+import (
+	"encoding/json"
+	"net/http"
+	"strconv"
+
+	"github.com/biruk-ak/clinical-lis/backend/internal/models"
+	"github.com/biruk-ak/clinical-lis/backend/internal/service"
+	"github.com/go-chi/chi/v5"
+)
+
+// DoctorRecord19Handler exposes REST endpoints for DoctorRecord19.
+type DoctorRecord19Handler struct {
+	svc *service.DoctorRecord19Service
+}
+
+// NewDoctorRecord19Handler constructs the HTTP handler.
+func NewDoctorRecord19Handler(svc *service.DoctorRecord19Service) *DoctorRecord19Handler {
+	return &DoctorRecord19Handler{svc: svc}
+}
+
+// Routes mounts doctors 19 routes under the given router.
+func (h *DoctorRecord19Handler) Routes(r chi.Router) {
+	r.Route("/doctors/v19", func(r chi.Router) {
+		r.Get("/", h.List)
+		r.Post("/", h.Create)
+		r.Get("/{id}", h.Get)
+		r.Put("/{id}", h.Update)
+		r.Delete("/{id}", h.Archive)
+		r.Get("/search", h.Search)
+		r.Get("/stats", h.Stats)
+	})
+}
+
+type createDoctorRecord19Request struct {
+	ExternalCode string `json:"external_code"`
+	DisplayName  string `json:"display_name"`
+	Status       string `json:"status"`
+	Priority     int    `json:"priority"`
+	FacilityID   string `json:"facility_id"`
+	Notes        string `json:"notes"`
+}
+
+// Create handles POST /doctors/v19.
+func (h *DoctorRecord19Handler) Create(w http.ResponseWriter, r *http.Request) {
+	var req createDoctorRecord19Request
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		writeError(w, http.StatusBadRequest, "invalid json")
+		return
+	}
+	actor := actorFrom(r)
+	rec, err := h.svc.Create(r.Context(), actor, req.FacilityID, req.ExternalCode, req.DisplayName, req.Status, req.Priority, req.Notes)
+	if err != nil {
+		writeError(w, http.StatusUnprocessableEntity, err.Error())
+		return
+	}
+	writeJSON(w, http.StatusCreated, rec)
+}
+
+// Get handles GET /doctors/v19/{id}.
+func (h *DoctorRecord19Handler) Get(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	rec, err := h.svc.Get(r.Context(), id)
+	if err == models.ErrNotFound {
+		writeError(w, http.StatusNotFound, "not found")
+		return
+	}
+	if err != nil {
+		writeError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	writeJSON(w, http.StatusOK, rec)
+}
+
+// List handles GET /doctors/v19.
+func (h *DoctorRecord19Handler) List(w http.ResponseWriter, r *http.Request) {
+	facilityID := r.URL.Query().Get("facility_id")
+	limit, _ := strconv.Atoi(r.URL.Query().Get("limit"))
+	offset, _ := strconv.Atoi(r.URL.Query().Get("offset"))
+	items, err := h.svc.List(r.Context(), facilityID, limit, offset)
+	if err != nil {
+		writeError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	writeJSON(w, http.StatusOK, map[string]any{"items": items})
+}
+
+// Update handles PUT /doctors/v19/{id}.
+func (h *DoctorRecord19Handler) Update(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	existing, err := h.svc.Get(r.Context(), id)
+	if err == models.ErrNotFound {
+		writeError(w, http.StatusNotFound, "not found")
+		return
+	}
+	if err != nil {
+		writeError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	var req createDoctorRecord19Request
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		writeError(w, http.StatusBadRequest, "invalid json")
+		return
+	}
+	existing.ExternalCode = req.ExternalCode
+	existing.DisplayName = req.DisplayName
+	existing.Status = req.Status
+	existing.Priority = req.Priority
+	existing.Notes = req.Notes
+	if err := h.svc.Update(r.Context(), actorFrom(r), existing); err != nil {
+		writeError(w, http.StatusConflict, err.Error())
+		return
+	}
+	writeJSON(w, http.StatusOK, existing)
+}
+
+// Archive handles DELETE /doctors/v19/{id}.
+func (h *DoctorRecord19Handler) Archive(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	if err := h.svc.Archive(r.Context(), id, actorFrom(r)); err == models.ErrNotFound {
+		writeError(w, http.StatusNotFound, "not found")
+		return
+	} else if err != nil {
+		writeError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	w.WriteHeader(http.StatusNoContent)
+}
+
+// Search handles GET /doctors/v19/search.
+func (h *DoctorRecord19Handler) Search(w http.ResponseWriter, r *http.Request) {
+	facilityID := r.URL.Query().Get("facility_id")
+	prefix := r.URL.Query().Get("q")
+	items, err := h.svc.Search(r.Context(), facilityID, prefix)
+	if err != nil {
+		writeError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	writeJSON(w, http.StatusOK, map[string]any{"items": items})
+}
+
+// Stats handles GET /doctors/v19/stats.
+func (h *DoctorRecord19Handler) Stats(w http.ResponseWriter, r *http.Request) {
+	facilityID := r.URL.Query().Get("facility_id")
+	n, err := h.svc.Stats(r.Context(), facilityID)
+	if err != nil {
+		writeError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	writeJSON(w, http.StatusOK, map[string]any{"active_count": n})
+}
---
+++ backend/internal/handler/doctors_20.go
@@ -0,0 +1,153 @@
+package handler
+
+
+import (
+	"encoding/json"
+	"net/http"
+	"strconv"
+
+	"github.com/biruk-ak/clinical-lis/backend/internal/models"
+	"github.com/biruk-ak/clinical-lis/backend/internal/service"
+	"github.com/go-chi/chi/v5"
+)
+
+// DoctorRecord20Handler exposes REST endpoints for DoctorRecord20.
+type DoctorRecord20Handler struct {
+	svc *service.DoctorRecord20Service
+}
+
+// NewDoctorRecord20Handler constructs the HTTP handler.
+func NewDoctorRecord20Handler(svc *service.DoctorRecord20Service) *DoctorRecord20Handler {
+	return &DoctorRecord20Handler{svc: svc}
+}
+
+// Routes mounts doctors 20 routes under the given router.
+func (h *DoctorRecord20Handler) Routes(r chi.Router) {
+	r.Route("/doctors/v20", func(r chi.Router) {
+		r.Get("/", h.List)
+		r.Post("/", h.Create)
+		r.Get("/{id}", h.Get)
+		r.Put("/{id}", h.Update)
+		r.Delete("/{id}", h.Archive)
+		r.Get("/search", h.Search)
+		r.Get("/stats", h.Stats)
+	})
+}
+
+type createDoctorRecord20Request struct {
+	ExternalCode string `json:"external_code"`
+	DisplayName  string `json:"display_name"`
+	Status       string `json:"status"`
+	Priority     int    `json:"priority"`
+	FacilityID   string `json:"facility_id"`
+	Notes        string `json:"notes"`
+}
+
+// Create handles POST /doctors/v20.
+func (h *DoctorRecord20Handler) Create(w http.ResponseWriter, r *http.Request) {
+	var req createDoctorRecord20Request
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		writeError(w, http.StatusBadRequest, "invalid json")
+		return
+	}
+	actor := actorFrom(r)
+	rec, err := h.svc.Create(r.Context(), actor, req.FacilityID, req.ExternalCode, req.DisplayName, req.Status, req.Priority, req.Notes)
+	if err != nil {
+		writeError(w, http.StatusUnprocessableEntity, err.Error())
+		return
+	}
+	writeJSON(w, http.StatusCreated, rec)
+}
+
+// Get handles GET /doctors/v20/{id}.
+func (h *DoctorRecord20Handler) Get(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	rec, err := h.svc.Get(r.Context(), id)
+	if err == models.ErrNotFound {
+		writeError(w, http.StatusNotFound, "not found")
+		return
+	}
+	if err != nil {
+		writeError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	writeJSON(w, http.StatusOK, rec)
+}
+
+// List handles GET /doctors/v20.
+func (h *DoctorRecord20Handler) List(w http.ResponseWriter, r *http.Request) {
+	facilityID := r.URL.Query().Get("facility_id")
+	limit, _ := strconv.Atoi(r.URL.Query().Get("limit"))
+	offset, _ := strconv.Atoi(r.URL.Query().Get("offset"))
+	items, err := h.svc.List(r.Context(), facilityID, limit, offset)
+	if err != nil {
+		writeError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	writeJSON(w, http.StatusOK, map[string]any{"items": items})
+}
+
+// Update handles PUT /doctors/v20/{id}.
+func (h *DoctorRecord20Handler) Update(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	existing, err := h.svc.Get(r.Context(), id)
+	if err == models.ErrNotFound {
+		writeError(w, http.StatusNotFound, "not found")
+		return
+	}
+	if err != nil {
+		writeError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	var req createDoctorRecord20Request
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		writeError(w, http.StatusBadRequest, "invalid json")
+		return
+	}
+	existing.ExternalCode = req.ExternalCode
+	existing.DisplayName = req.DisplayName
+	existing.Status = req.Status
+	existing.Priority = req.Priority
+	existing.Notes = req.Notes
+	if err := h.svc.Update(r.Context(), actorFrom(r), existing); err != nil {
+		writeError(w, http.StatusConflict, err.Error())
+		return
+	}
+	writeJSON(w, http.StatusOK, existing)
+}
+
+// Archive handles DELETE /doctors/v20/{id}.
+func (h *DoctorRecord20Handler) Archive(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	if err := h.svc.Archive(r.Context(), id, actorFrom(r)); err == models.ErrNotFound {
+		writeError(w, http.StatusNotFound, "not found")
+		return
+	} else if err != nil {
+		writeError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	w.WriteHeader(http.StatusNoContent)
+}
+
+// Search handles GET /doctors/v20/search.
+func (h *DoctorRecord20Handler) Search(w http.ResponseWriter, r *http.Request) {
+	facilityID := r.URL.Query().Get("facility_id")
+	prefix := r.URL.Query().Get("q")
+	items, err := h.svc.Search(r.Context(), facilityID, prefix)
+	if err != nil {
+		writeError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	writeJSON(w, http.StatusOK, map[string]any{"items": items})
+}
+
+// Stats handles GET /doctors/v20/stats.
+func (h *DoctorRecord20Handler) Stats(w http.ResponseWriter, r *http.Request) {
+	facilityID := r.URL.Query().Get("facility_id")
+	n, err := h.svc.Stats(r.Context(), facilityID)
+	if err != nil {
+		writeError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	writeJSON(w, http.StatusOK, map[string]any{"active_count": n})
+}
