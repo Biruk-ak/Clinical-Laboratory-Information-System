05c6ef4da4c44414fa3d8d0fb5b7da2dcc4ae236
Biruk-ak <birukaklilu0110@gmail.com>
2022-05-08 10:20:00 +0300

    Add Patients demographics domain models, migrations, and docs

---
+++ backend/migrations/patients_17.sql
@@ -0,0 +1,24 @@
+-- Migration for patients slice 17
+CREATE TABLE IF NOT EXISTS patients_17_records (
+  id            TEXT PRIMARY KEY,
+  external_code TEXT NOT NULL,
+  display_name  TEXT NOT NULL,
+  status        TEXT NOT NULL,
+  priority      INTEGER NOT NULL DEFAULT 0,
+  facility_id   TEXT NOT NULL,
+  created_by    TEXT NOT NULL,
+  updated_by    TEXT NOT NULL,
+  notes         TEXT NOT NULL DEFAULT '',
+  metadata_json TEXT NOT NULL DEFAULT '{}',
+  version       BIGINT NOT NULL DEFAULT 1,
+  is_active     BOOLEAN NOT NULL DEFAULT TRUE,
+  created_at    TIMESTAMPTZ NOT NULL,
+  updated_at    TIMESTAMPTZ NOT NULL,
+  archived_at   TIMESTAMPTZ NULL
+);
+
+CREATE INDEX IF NOT EXISTS idx_patients_17_facility_active
+  ON patients_17_records (facility_id, is_active, updated_at DESC);
+
+CREATE INDEX IF NOT EXISTS idx_patients_17_external_code
+  ON patients_17_records (facility_id, external_code);
---
+++ docs/modules/patients.md
@@ -0,0 +1,5 @@
+# Patient module
+
+Demographics, identifiers, and encounter linkage
+
+Generated domain slices: 28
