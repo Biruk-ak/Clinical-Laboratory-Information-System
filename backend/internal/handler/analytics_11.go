55f23e025d39fa411df5724f7698b75e4b104b36
Biruk-ak <birukaklilu0110@gmail.com>
2022-05-26 12:20:00 +0300

    Expose Analytics KPIs REST API handlers

---
+++ backend/internal/handler/analytics_11.go
@@ -0,0 +1,153 @@
+package handler
+
+
+import (
+	"encoding/json"
+	"net/http"
+	"strconv"
+
+	"github.com/biruk-ak/clinical-lis/backend/internal/models"
+	"github.com/biruk-ak/clinical-lis/backend/internal/service"
+	"github.com/go-chi/chi/v5"
+)
+
+// AnalyticsRecord11Handler exposes REST endpoints for AnalyticsRecord11.
+type AnalyticsRecord11Handler struct {
+	svc *service.AnalyticsRecord11Service
+}
+
+// NewAnalyticsRecord11Handler constructs the HTTP handler.
+func NewAnalyticsRecord11Handler(svc *service.AnalyticsRecord11Service) *AnalyticsRecord11Handler {
+	return &AnalyticsRecord11Handler{svc: svc}
+}
+
+// Routes mounts analytics 11 routes under the given router.
+func (h *AnalyticsRecord11Handler) Routes(r chi.Router) {
+	r.Route("/analytics/v11", func(r chi.Router) {
+		r.Get("/", h.List)
+		r.Post("/", h.Create)
+		r.Get("/{id}", h.Get)
+		r.Put("/{id}", h.Update)
+		r.Delete("/{id}", h.Archive)
+		r.Get("/search", h.Search)
+		r.Get("/stats", h.Stats)
+	})
+}
+
+type createAnalyticsRecord11Request struct {
+	ExternalCode string `json:"external_code"`
+	DisplayName  string `json:"display_name"`
+	Status       string `json:"status"`
+	Priority     int    `json:"priority"`
+	FacilityID   string `json:"facility_id"`
+	Notes        string `json:"notes"`
+}
+
+// Create handles POST /analytics/v11.
+func (h *AnalyticsRecord11Handler) Create(w http.ResponseWriter, r *http.Request) {
+	var req createAnalyticsRecord11Request
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		writeError(w, http.StatusBadRequest, "invalid json")
+		return
+	}
+	actor := actorFrom(r)
+	rec, err := h.svc.Create(r.Context(), actor, req.FacilityID, req.ExternalCode, req.DisplayName, req.Status, req.Priority, req.Notes)
+	if err != nil {
+		writeError(w, http.StatusUnprocessableEntity, err.Error())
+		return
+	}
+	writeJSON(w, http.StatusCreated, rec)
+}
+
+// Get handles GET /analytics/v11/{id}.
+func (h *AnalyticsRecord11Handler) Get(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	rec, err := h.svc.Get(r.Context(), id)
+	if err == models.ErrNotFound {
+		writeError(w, http.StatusNotFound, "not found")
+		return
+	}
+	if err != nil {
+		writeError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	writeJSON(w, http.StatusOK, rec)
+}
+
+// List handles GET /analytics/v11.
+func (h *AnalyticsRecord11Handler) List(w http.ResponseWriter, r *http.Request) {
+	facilityID := r.URL.Query().Get("facility_id")
+	limit, _ := strconv.Atoi(r.URL.Query().Get("limit"))
+	offset, _ := strconv.Atoi(r.URL.Query().Get("offset"))
+	items, err := h.svc.List(r.Context(), facilityID, limit, offset)
+	if err != nil {
+		writeError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	writeJSON(w, http.StatusOK, map[string]any{"items": items})
+}
+
+// Update handles PUT /analytics/v11/{id}.
+func (h *AnalyticsRecord11Handler) Update(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	existing, err := h.svc.Get(r.Context(), id)
+	if err == models.ErrNotFound {
+		writeError(w, http.StatusNotFound, "not found")
+		return
+	}
+	if err != nil {
+		writeError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	var req createAnalyticsRecord11Request
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		writeError(w, http.StatusBadRequest, "invalid json")
+		return
+	}
+	existing.ExternalCode = req.ExternalCode
+	existing.DisplayName = req.DisplayName
+	existing.Status = req.Status
+	existing.Priority = req.Priority
+	existing.Notes = req.Notes
+	if err := h.svc.Update(r.Context(), actorFrom(r), existing); err != nil {
+		writeError(w, http.StatusConflict, err.Error())
+		return
+	}
+	writeJSON(w, http.StatusOK, existing)
+}
+
+// Archive handles DELETE /analytics/v11/{id}.
+func (h *AnalyticsRecord11Handler) Archive(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	if err := h.svc.Archive(r.Context(), id, actorFrom(r)); err == models.ErrNotFound {
+		writeError(w, http.StatusNotFound, "not found")
+		return
+	} else if err != nil {
+		writeError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	w.WriteHeader(http.StatusNoContent)
+}
+
+// Search handles GET /analytics/v11/search.
+func (h *AnalyticsRecord11Handler) Search(w http.ResponseWriter, r *http.Request) {
+	facilityID := r.URL.Query().Get("facility_id")
+	prefix := r.URL.Query().Get("q")
+	items, err := h.svc.Search(r.Context(), facilityID, prefix)
+	if err != nil {
+		writeError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	writeJSON(w, http.StatusOK, map[string]any{"items": items})
+}
+
+// Stats handles GET /analytics/v11/stats.
+func (h *AnalyticsRecord11Handler) Stats(w http.ResponseWriter, r *http.Request) {
+	facilityID := r.URL.Query().Get("facility_id")
+	n, err := h.svc.Stats(r.Context(), facilityID)
+	if err != nil {
+		writeError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	writeJSON(w, http.StatusOK, map[string]any{"active_count": n})
+}
