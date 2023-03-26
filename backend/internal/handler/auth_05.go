59019e252ad1c4d19b9489fa1f833db3febfb7fc
Biruk-ak <birukaklilu0110@gmail.com>
2023-03-26 12:20:00 +0300

    Expose Auth and roles REST API handlers

---
+++ backend/internal/handler/auth_05.go
@@ -0,0 +1,153 @@
+package handler
+
+
+import (
+	"encoding/json"
+	"net/http"
+	"strconv"
+
+	"github.com/biruk-ak/clinical-lis/backend/internal/models"
+	"github.com/biruk-ak/clinical-lis/backend/internal/service"
+	"github.com/go-chi/chi/v5"
+)
+
+// AuthRecord05Handler exposes REST endpoints for AuthRecord05.
+type AuthRecord05Handler struct {
+	svc *service.AuthRecord05Service
+}
+
+// NewAuthRecord05Handler constructs the HTTP handler.
+func NewAuthRecord05Handler(svc *service.AuthRecord05Service) *AuthRecord05Handler {
+	return &AuthRecord05Handler{svc: svc}
+}
+
+// Routes mounts auth 05 routes under the given router.
+func (h *AuthRecord05Handler) Routes(r chi.Router) {
+	r.Route("/auth/v05", func(r chi.Router) {
+		r.Get("/", h.List)
+		r.Post("/", h.Create)
+		r.Get("/{id}", h.Get)
+		r.Put("/{id}", h.Update)
+		r.Delete("/{id}", h.Archive)
+		r.Get("/search", h.Search)
+		r.Get("/stats", h.Stats)
+	})
+}
+
+type createAuthRecord05Request struct {
+	ExternalCode string `json:"external_code"`
+	DisplayName  string `json:"display_name"`
+	Status       string `json:"status"`
+	Priority     int    `json:"priority"`
+	FacilityID   string `json:"facility_id"`
+	Notes        string `json:"notes"`
+}
+
+// Create handles POST /auth/v05.
+func (h *AuthRecord05Handler) Create(w http.ResponseWriter, r *http.Request) {
+	var req createAuthRecord05Request
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		writeError(w, http.StatusBadRequest, "invalid json")
+		return
+	}
+	actor := actorFrom(r)
+	rec, err := h.svc.Create(r.Context(), actor, req.FacilityID, req.ExternalCode, req.DisplayName, req.Status, req.Priority, req.Notes)
+	if err != nil {
+		writeError(w, http.StatusUnprocessableEntity, err.Error())
+		return
+	}
+	writeJSON(w, http.StatusCreated, rec)
+}
+
+// Get handles GET /auth/v05/{id}.
+func (h *AuthRecord05Handler) Get(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	rec, err := h.svc.Get(r.Context(), id)
+	if err == models.ErrNotFound {
+		writeError(w, http.StatusNotFound, "not found")
+		return
+	}
+	if err != nil {
+		writeError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	writeJSON(w, http.StatusOK, rec)
+}
+
+// List handles GET /auth/v05.
+func (h *AuthRecord05Handler) List(w http.ResponseWriter, r *http.Request) {
+	facilityID := r.URL.Query().Get("facility_id")
+	limit, _ := strconv.Atoi(r.URL.Query().Get("limit"))
+	offset, _ := strconv.Atoi(r.URL.Query().Get("offset"))
+	items, err := h.svc.List(r.Context(), facilityID, limit, offset)
+	if err != nil {
+		writeError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	writeJSON(w, http.StatusOK, map[string]any{"items": items})
+}
+
+// Update handles PUT /auth/v05/{id}.
+func (h *AuthRecord05Handler) Update(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	existing, err := h.svc.Get(r.Context(), id)
+	if err == models.ErrNotFound {
+		writeError(w, http.StatusNotFound, "not found")
+		return
+	}
+	if err != nil {
+		writeError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	var req createAuthRecord05Request
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		writeError(w, http.StatusBadRequest, "invalid json")
+		return
+	}
+	existing.ExternalCode = req.ExternalCode
+	existing.DisplayName = req.DisplayName
+	existing.Status = req.Status
+	existing.Priority = req.Priority
+	existing.Notes = req.Notes
+	if err := h.svc.Update(r.Context(), actorFrom(r), existing); err != nil {
+		writeError(w, http.StatusConflict, err.Error())
+		return
+	}
+	writeJSON(w, http.StatusOK, existing)
+}
+
+// Archive handles DELETE /auth/v05/{id}.
+func (h *AuthRecord05Handler) Archive(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	if err := h.svc.Archive(r.Context(), id, actorFrom(r)); err == models.ErrNotFound {
+		writeError(w, http.StatusNotFound, "not found")
+		return
+	} else if err != nil {
+		writeError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	w.WriteHeader(http.StatusNoContent)
+}
+
+// Search handles GET /auth/v05/search.
+func (h *AuthRecord05Handler) Search(w http.ResponseWriter, r *http.Request) {
+	facilityID := r.URL.Query().Get("facility_id")
+	prefix := r.URL.Query().Get("q")
+	items, err := h.svc.Search(r.Context(), facilityID, prefix)
+	if err != nil {
+		writeError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	writeJSON(w, http.StatusOK, map[string]any{"items": items})
+}
+
+// Stats handles GET /auth/v05/stats.
+func (h *AuthRecord05Handler) Stats(w http.ResponseWriter, r *http.Request) {
+	facilityID := r.URL.Query().Get("facility_id")
+	n, err := h.svc.Stats(r.Context(), facilityID)
+	if err != nil {
+		writeError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	writeJSON(w, http.StatusOK, map[string]any{"active_count": n})
+}
---
+++ backend/internal/handler/auth_09.go
@@ -0,0 +1,153 @@
+package handler
+
+
+import (
+	"encoding/json"
+	"net/http"
+	"strconv"
+
+	"github.com/biruk-ak/clinical-lis/backend/internal/models"
+	"github.com/biruk-ak/clinical-lis/backend/internal/service"
+	"github.com/go-chi/chi/v5"
+)
+
+// AuthRecord09Handler exposes REST endpoints for AuthRecord09.
+type AuthRecord09Handler struct {
+	svc *service.AuthRecord09Service
+}
+
+// NewAuthRecord09Handler constructs the HTTP handler.
+func NewAuthRecord09Handler(svc *service.AuthRecord09Service) *AuthRecord09Handler {
+	return &AuthRecord09Handler{svc: svc}
+}
+
+// Routes mounts auth 09 routes under the given router.
+func (h *AuthRecord09Handler) Routes(r chi.Router) {
+	r.Route("/auth/v09", func(r chi.Router) {
+		r.Get("/", h.List)
+		r.Post("/", h.Create)
+		r.Get("/{id}", h.Get)
+		r.Put("/{id}", h.Update)
+		r.Delete("/{id}", h.Archive)
+		r.Get("/search", h.Search)
+		r.Get("/stats", h.Stats)
+	})
+}
+
+type createAuthRecord09Request struct {
+	ExternalCode string `json:"external_code"`
+	DisplayName  string `json:"display_name"`
+	Status       string `json:"status"`
+	Priority     int    `json:"priority"`
+	FacilityID   string `json:"facility_id"`
+	Notes        string `json:"notes"`
+}
+
+// Create handles POST /auth/v09.
+func (h *AuthRecord09Handler) Create(w http.ResponseWriter, r *http.Request) {
+	var req createAuthRecord09Request
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		writeError(w, http.StatusBadRequest, "invalid json")
+		return
+	}
+	actor := actorFrom(r)
+	rec, err := h.svc.Create(r.Context(), actor, req.FacilityID, req.ExternalCode, req.DisplayName, req.Status, req.Priority, req.Notes)
+	if err != nil {
+		writeError(w, http.StatusUnprocessableEntity, err.Error())
+		return
+	}
+	writeJSON(w, http.StatusCreated, rec)
+}
+
+// Get handles GET /auth/v09/{id}.
+func (h *AuthRecord09Handler) Get(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	rec, err := h.svc.Get(r.Context(), id)
+	if err == models.ErrNotFound {
+		writeError(w, http.StatusNotFound, "not found")
+		return
+	}
+	if err != nil {
+		writeError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	writeJSON(w, http.StatusOK, rec)
+}
+
+// List handles GET /auth/v09.
+func (h *AuthRecord09Handler) List(w http.ResponseWriter, r *http.Request) {
+	facilityID := r.URL.Query().Get("facility_id")
+	limit, _ := strconv.Atoi(r.URL.Query().Get("limit"))
+	offset, _ := strconv.Atoi(r.URL.Query().Get("offset"))
+	items, err := h.svc.List(r.Context(), facilityID, limit, offset)
+	if err != nil {
+		writeError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	writeJSON(w, http.StatusOK, map[string]any{"items": items})
+}
+
+// Update handles PUT /auth/v09/{id}.
+func (h *AuthRecord09Handler) Update(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	existing, err := h.svc.Get(r.Context(), id)
+	if err == models.ErrNotFound {
+		writeError(w, http.StatusNotFound, "not found")
+		return
+	}
+	if err != nil {
+		writeError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	var req createAuthRecord09Request
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		writeError(w, http.StatusBadRequest, "invalid json")
+		return
+	}
+	existing.ExternalCode = req.ExternalCode
+	existing.DisplayName = req.DisplayName
+	existing.Status = req.Status
+	existing.Priority = req.Priority
+	existing.Notes = req.Notes
+	if err := h.svc.Update(r.Context(), actorFrom(r), existing); err != nil {
+		writeError(w, http.StatusConflict, err.Error())
+		return
+	}
+	writeJSON(w, http.StatusOK, existing)
+}
+
+// Archive handles DELETE /auth/v09/{id}.
+func (h *AuthRecord09Handler) Archive(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	if err := h.svc.Archive(r.Context(), id, actorFrom(r)); err == models.ErrNotFound {
+		writeError(w, http.StatusNotFound, "not found")
+		return
+	} else if err != nil {
+		writeError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	w.WriteHeader(http.StatusNoContent)
+}
+
+// Search handles GET /auth/v09/search.
+func (h *AuthRecord09Handler) Search(w http.ResponseWriter, r *http.Request) {
+	facilityID := r.URL.Query().Get("facility_id")
+	prefix := r.URL.Query().Get("q")
+	items, err := h.svc.Search(r.Context(), facilityID, prefix)
+	if err != nil {
+		writeError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	writeJSON(w, http.StatusOK, map[string]any{"items": items})
+}
+
+// Stats handles GET /auth/v09/stats.
+func (h *AuthRecord09Handler) Stats(w http.ResponseWriter, r *http.Request) {
+	facilityID := r.URL.Query().Get("facility_id")
+	n, err := h.svc.Stats(r.Context(), facilityID)
+	if err != nil {
+		writeError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	writeJSON(w, http.StatusOK, map[string]any{"active_count": n})
+}
